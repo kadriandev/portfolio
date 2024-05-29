0214e9d65af9a5a61e342ce179fbfdd03284f00a
kadriandev <kyle.monteiro@gmail.com>
2024-05-29 02:00:58 -0400

    Added particle background, projects page, and made mobile friendly

--- components/experience-card.tsx
+++ components/experience-card.tsx
@@ -1,105 +1,127 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
-import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "./ui/drawer";
-import { ReactNode } from 'react';
-import { Badge } from './ui/badge';
+import {
+  Drawer,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerClose,
+} from "./ui/drawer";
+import { ReactNode } from "react";
+import { Badge } from "./ui/badge";
 
 const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 export interface ExperienceCardProps {
-    company: string;
-    address: string;
-    imageSrc: string;
-    jobTitle: string;
-    from: Date;
-    to: Date;
-    skills?: Array<string>;
-    children: ReactNode | Array<ReactNode>
+  company: string;
+  address: string;
+  imageSrc: string;
+  jobTitle: string;
+  from: Date;
+  to: Date;
+  skills?: Array<string>;
+  children: ReactNode | Array<ReactNode>;
 }
 
 export function ExperienceCard({
-    company,
-    address,
-    imageSrc,
-    jobTitle,
-    from,
-    to,
-    skills = [],
-    children
+  company,
+  address,
+  imageSrc,
+  jobTitle,
+  from,
+  to,
+  skills = [],
+  children,
 }: ExperienceCardProps) {
+  const getDate = (date: Date) =>
+    date.setHours(0, 0, 0, 0) != new Date().setHours(0, 0, 0, 0)
+      ? `${months[date.getMonth()]} ${date.getFullYear()}`
+      : "Present";
 
-    const getDate = (date: Date) => date.setHours(0, 0, 0, 0) != new Date().setHours(0, 0, 0, 0) ?
-        `${months[date.getMonth()]} ${date.getFullYear()}` : 'Present';
-
-    return (
-        <Drawer>
-            <DrawerTrigger className="shadow-2xl odd:shadow-violet-500 even:shadow-teal-500">
-                <Card className={`h-80 bg-stone-800 min-w-[350px] max-w-[375px] scale-[1.02] hover:scale-110 transition-transform`}>
-                    <CardContent className="p-0">
-                        <Image
-                            className="rounded-t-2xl h-[175px]"
-                            src={imageSrc}
-                            alt={`${company} Logo`}
-                            width={600}
-                            height={300}
-                        />
-                        <div className="p-4 mt-6">
-                            <CardTitle className="mb-2">{jobTitle}</CardTitle>
-                            <CardDescription>{`${getDate(from)} - ${getDate(to)}`}</CardDescription>
-                        </div>
-                    </CardContent>
-                </Card>
-            </DrawerTrigger>
-            <DrawerContent>
-                <div className="mx-auto w-full max-w-full">
-                    <DrawerHeader>
-                        <Image
-                            className="rounded-2xl mb-4 w-full"
-                            src={imageSrc}
-                            alt={`${company} Logo`}
-                            width={500}
-                            height={300}
-                        />
-                        <DrawerTitle className="text-center">{`${jobTitle} at ${company}`}</DrawerTitle>
-                        <DrawerDescription className="text-center">
-                            {`${getDate(from)} - ${getDate(to)}, ${address}`}
-                        </DrawerDescription>
-                    </DrawerHeader>
-                    <div className={"px-3 grid grid-cols-[3fr_1fr] gap-x-8"}>
-                        <h2 className="font-semibold mb-4">Description</h2>
-                        <h2 className="font-semibold mb-4">Skills</h2>
-                        <div className={"[&>*]:mb-4"}>
-                            {children}
-                        </div>
-                        <div>
-                            {skills.map(s => (
-                                <Badge
-                                    key={s}
-                                    className="m-1 bg-transparent border-white text-white hover:none"
-                                >
-                                    {s}
-                                </Badge>
-                            ))}
-                        </div>
-                    </div>
-                    <DrawerFooter>
-                        <DrawerClose>Close</DrawerClose>
-                    </DrawerFooter>
-                </div>
-            </DrawerContent>
-        </Drawer>
-    );
+  return (
+    <Drawer>
+      <DrawerTrigger className="shadow-2xl odd:shadow-violet-500 even:shadow-teal-500">
+        <Card
+          className={`h-80 bg-stone-800 min-w-[325px] max-w-[350px] scale-[1.02] hover:scale-110 transition-transform`}
+        >
+          <CardContent className="p-0">
+            <Image
+              className="rounded-t-2xl h-[175px]"
+              src={imageSrc}
+              alt={`${company} Logo`}
+              width={600}
+              height={300}
+            />
+            <div className="p-4 mt-6">
+              <CardTitle className="mb-2">{jobTitle}</CardTitle>
+              <CardDescription>{`${getDate(from)} - ${getDate(
+                to,
+              )}`}</CardDescription>
+            </div>
+          </CardContent>
+        </Card>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="md:mx-auto w-full max-w-full">
+          <DrawerHeader>
+            <Image
+              className="rounded-2xl mb-4 w-full"
+              src={imageSrc}
+              alt={`${company} Logo`}
+              width={500}
+              height={300}
+            />
+            <DrawerTitle className="text-center text-white">{`${jobTitle} at ${company}`}</DrawerTitle>
+            <DrawerDescription className="text-center">
+              {`${getDate(from)} - ${getDate(to)}, ${address}`}
+            </DrawerDescription>
+          </DrawerHeader>
+          <div className={"md:px-3 md:grid md:grid-cols-[3fr_1fr] md:gap-x-8"}>
+            <h2 className="font-semibold mb-4 hidden md:block">Description</h2>
+            <h2 className="font-semibold mb-4 hidden md:block">Skills</h2>
+            <div className="my-2 md:my-0 block md:hidden">
+              {skills.map((s) => (
+                <Badge
+                  key={s}
+                  className="m-1 bg-transparent border-white text-white hover:none"
+                >
+                  {s}
+                </Badge>
+              ))}
+            </div>
+            <div className={"[&>*]:mb-4 text-white"}>{children}</div>
+            <div className="my-2 md:my-0 hidden md:block">
+              {skills.map((s) => (
+                <Badge
+                  key={s}
+                  className="m-1 bg-transparent border-white text-white hover:none"
+                >
+                  {s}
+                </Badge>
+              ))}
+            </div>
+          </div>
+          <DrawerFooter>
+            <DrawerClose>Close</DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 }
---
+++ components/github-repo-card.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardDescription,
+} from "./ui/card";
+import { Star } from "lucide-react";
+import { Badge } from "./ui/badge";
+
+interface GithubRepoCardProps {
+  repo: string;
+}
+
+export default async function GithubRepoCard(props: GithubRepoCardProps) {
+  const { repo } = props;
+
+  const [repoData, languages] = await Promise.all([
+    fetch(`http://api.github.com/repos/${repo}`).then((res) => res.json()),
+    fetch(`http://api.github.com/repos/${repo}/languages`)
+      .then((res) => res.json())
+      .then((res) => Object.entries(res)),
+  ]);
+  return (
+    <Link href={repoData.html_url}>
+      <Card className="h-full hover:bg-card/30 hover:scale-105 transition-transform">
+        <CardHeader>
+          <CardTitle className="flex">
+            {repoData.full_name}
+            <span className="flex gap-1 ml-auto font-normal">
+              <Star />
+              {repoData.stargazers_count}
+            </span>
+          </CardTitle>
+          <CardDescription>{repoData.description}</CardDescription>
+          <CardContent className="mx-0 px-0 py-1 flex gap-1">
+            {languages.map(([lang, lines]) => (
+              <Badge
+                key={lang}
+                className="bg-transparent text-white border-white hover:none"
+              >
+                {lang}
+              </Badge>
+            ))}
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </Link>
+  );
+}
